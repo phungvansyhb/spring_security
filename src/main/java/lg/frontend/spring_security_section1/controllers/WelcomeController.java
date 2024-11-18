d47769d6dd3f26310a176b77072f6880286234dd
Phung Van Sy,Michel <phungvansyhbfw@lgcns.com>
2024-11-18 16:01:29 +0700

    init project security

---
+++ src/main/java/lg/frontend/spring_security_section1/controllers/WelcomeController.java
@@ -0,0 +1,12 @@
+package lg.frontend.spring_security_section1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome gello";
+    }
+}
---
+++ src/test/java/lg/frontend/spring_security_section1/SpringSecuritySection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lg.frontend.spring_security_section1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
